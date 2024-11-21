d25667019a00f1c2623b0c9a8cde397bafe27e76
hsh9865 <20181604@edu.hanbat.ac.kr>
2024-11-21 13:18:32 +0900

    1

---
+++ 004 Project/Assets/_Scripts/teleport.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject portal;
+
+    void Start()
+    {
+    }
+    public void tele()
+    {
+        
+        player = GameObject.Find("Player");
+        portal = GameObject.Find("Portal(Clone)");
+        
+        if(player ==null || portal==null)
+        {
+            Debug.Log(1);
+        }
+        player.transform.position = portal.transform.position;
+    }
+    
+}
